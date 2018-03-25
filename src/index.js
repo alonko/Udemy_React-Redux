8bd68d4d16d493f3a2a2ac5c2d33072a33e97801
Alon Kodner <alon.kodner@bosun.co.nz>
2018-03-25 20:29:37 +1300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KRY = "AIzaSyAtBUGJtCrW9SxAfb_vHfnFTq0ZiHMWXyk";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
+} 
+ReactDOM.render(<App />, document.querySelector('.container'));
