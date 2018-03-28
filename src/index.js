0492a152a4006841be4e4c37d193c4dec7a197ab
Alon Kodner <alon.kodner@bosun.co.nz>
2018-03-28 21:35:59 +1300

    changes up to lecture 28

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details'
 
-const API_KRY = "AIzaSyAtBUGJtCrW9SxAfb_vHfnFTq0ZiHMWXyk";
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
-} 
+const API_KEY = "AIzaSyAtBUGJtCrW9SxAfb_vHfnFTq0ZiHMWXyk";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            console.log(videos);
+            this.setState({videos}); // = this.setState({videos: videos});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 ReactDOM.render(<App />, document.querySelector('.container'));
